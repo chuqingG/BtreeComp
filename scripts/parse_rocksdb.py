f96ec773505a8b11df697b56aee6120560c3984e
chuqingG <gao688@purdue.edu>
2023-09-10 19:16:44 +0000

    rm scripts

--- scripts/parse_rocksdb.py
+++
@@ -1,9 +0,0 @@
-with open('/Users/shreyaballijepalli//Downloads/keys_random_20M.txt') as f:
-    data = f.readlines()
-
-data = data[:10000000]
-print(len(data))
-parsed_data = []
-for val in data:
-    print(len(val))
-    break
--- scripts/parse_tpch.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_table('../../../datasets/tpch/customer.tbl',delimiter='|', header=None)
-# combined_df = df.iloc[:, 1:].apply(lambda x: "|".join(x.astype(str)), axis=1)
-combined_df = df.iloc[:, 1:]
-combined_df.to_csv('../../../datasets/tpch/customer_from1_300.txt', header=None, index=None, sep='|', mode='a')
-

ded9b20365a79876502d9e2f73949cdc563a9fa6
Sikang Sun <sun1017@data.cs.purdue.edu>
2024-05-08 19:36:41 -0400

    pv compare

--- utils/compare.cpp
+++ utils/compare.cpp
@@ -28,9 +28,15 @@ int char_cmp_skip(const char *a, const char *b,
     return (alen - blen);
 }
 
-int char_cmp_new(const char *a, const char *b, int alen, int blen) {
+int char_cmp_new(const char *a, const char *b, int alen, int blen) { // gauruntee to be longer than 4 in PV. Handles is 4 as well.
     // 1 : a > b
     // const size_t min_len = (alen < blen) ? alen : blen;
+    #ifdef PV
+    const char* a = a + 4;
+    const char* b = b + 4;
+    alen -= 4;
+    blen -= 4;   
+    #endif
     int cmp_len = min(alen, blen);
     // int idx = *matchp;
     for (int idx = 0; idx < cmp_len; ++idx) {

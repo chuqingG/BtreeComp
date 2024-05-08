8d55e9372e2e601b7d39c99096817586e792b80e
Sikang Sun <sun1017@data.cs.purdue.edu>
2024-05-08 02:10:20 -0400

    a

--- tree/btree_std.h
+++ tree/btree_std.h
@@ -44,7 +44,7 @@ private:
                         splitReturn_new *childsplit);
     int search_insert_pos(Node *cursor, const char *key, int keylen, int low, int high,
                           bool &equal);
-    int word_cmp(Stdhead* header,const char* key, int keylen);
+    long word_cmp(Stdhead* header,const char* key, int keylen);
 
     long search_in_node(Node *cursor, const char *key, int keylen, int low, int high, bool isleaf);
     Node *search_leaf_node(Node *root, const char *key, int keylen);

c3af4b6a093d2a638877e524d5449eb471f4d50b
Sikang Sun <sun1017@data.cs.purdue.edu>
2024-05-15 04:30:46 -0400

    workflow fixes

--- tree/btree_std.cpp
+++ tree/btree_std.cpp
@@ -191,8 +191,8 @@ void BPTree::insert_nonleaf(Node *node, Node **path,
         bool equal = false;
 
 #ifdef KN
-        char * convkey = string_conv(childsplit->promotekey->addr, childsplit->promotekey->size, node->prefix->size); //
-        promotekey->addr = convkey;
+        char * convkey = string_conv(newkey->addr, newkey->size, node->prefix->size); //
+        newkey->addr = convkey;
 #endif
         if (this->head_comp) {
 #ifdef KN

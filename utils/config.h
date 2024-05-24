085238ed2004ee8e3b5505f374e5232f8464aecd
Sikang Sun <sun1017@data.cs.purdue.edu>
2024-05-24 19:31:30 -0400

    ubs2 check:

--- utils/config.h
+++ utils/config.h
@@ -9,7 +9,7 @@
 #define CHARALL
 #define SINGLE_DEBUG
 #define CHECK
-#define PRINT //enable checking warmup and print warmup tree
+// #define PRINT //enable checking warmup and print warmup tree
 // #define VERBOSE_PRINT // enable to write some intermediate results to output file (if set -o)
 // #define WT_OPTIM   // enable for prefixstart
 // #define WTCACHE

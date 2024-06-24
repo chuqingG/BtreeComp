660356c6b76732d8c311d53b12861cf1a80d0781
Sikang Sun <sun1017@mc18.cs.purdue.edu>
2024-06-24 19:38:00 -0400

    inline

--- tree/node_inline.h
+++ tree/node_inline.h
@@ -159,7 +159,7 @@ inline long word_cmp(Stdhead* header,const char* key, int keylen) {
     // return result;
 }
 
-long pvComp(Stdhead* header,const char* key, int keylen, Node *cursor) {
+inline long pvComp(Stdhead* header,const char* key, int keylen, Node *cursor) {
     long cmp = word_cmp(header, key, keylen);
     if (cmp == 0) {
         cmp = char_cmp_new(key, PageOffset(cursor, header->key_offset),

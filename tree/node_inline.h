166262ec7420e4a7954d4cdea3697cac029cfbb0
Sikang Sun <sun1017@mc18.cs.purdue.edu>
2024-06-27 15:25:44 -0400

    inline assembly

--- tree/node_inline.h
+++ tree/node_inline.h
@@ -200,10 +200,10 @@ inline int unrolledBinarySearch(Node *cursor, const char *key, int keylen, long 
       local_cmp = pvComp(first - length, key, keylen, cursor);
       long first_temp;
         asm volatile (
-            "testq %[local_cmp], %[local_cmp]\n\t"        // Test local_cmp with itself to set flags
-            "addq %[length], %[rem]\n\t"                 // Calculate length + rem and store result in rem (overwrite rem)
+            "test %[local_cmp], %[local_cmp]\n\t"        // Test local_cmp with itself to set flags
+            "addl %[length], %[rem]\n\t"                 // Calculate length + rem and store result in rem (overwrite rem)
             "movq %[first], %[first_temp]\n\t"           // Move first into first_temp
-            "subq %[rem], %[first_temp]\n\t"             // Subtract rem from first_temp
+            "subl %[rem], %[first_temp]\n\t"             // Subtract rem from first_temp
             "cmovge %[first], %[first_temp]\n\t"        // If local_cmp >= 0, move first_temp to first
             : [first] "+r" (first), [first_temp] "=&r" (first_temp), [rem] "+r" (rem)  // Output operands
             : [local_cmp] "r" (local_cmp), [length] "r" (length)  // Input operands
